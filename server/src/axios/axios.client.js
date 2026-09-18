ea371ff0247d09aecafb4cda56808b84deb8f762
hachizeus <harchijohns01@gmail.com>
2026-09-18 16:14:42 +0300

    auto-save: update $(date +%s)

--- server/src/axios/axios.client.js
+++ server/src/axios/axios.client.js
@@ -1,13 +1,21 @@
 import axios from "axios";
+import tmdbConfig from "../tmdb/tmdb.config.js";
 
 const get = async (url) => {
-  const response = await axios.get(url, {
-    headers: {
-      Accept: "application/json",
-      "Accept-Encoding": "identity"
-    }
-  });
-  return response.data;
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${tmdbConfig.token}`,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Accept-Encoding": "identity"
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`Axios request failed: ${error.message}`, { url });
+    throw error;
+  }
 };
 
 export default { get };
