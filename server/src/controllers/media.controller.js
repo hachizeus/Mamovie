a75692ffe6449413cfbd54d1be08994490cef999
hachizeus <harchijohns01@gmail.com>
2026-09-18 16:44:55 +0300

    auto-save: update $(date +%s)

--- server/src/controllers/media.controller.js
+++ server/src/controllers/media.controller.js
@@ -139,11 +139,8 @@ const getDetail = async (req, res) => {
   try {
     const { mediaType, mediaId } = req.params;
 
-    // Validate mediaType
-    if (!mediaType || !["movie", "tv"].includes(mediaType)) {
-      return responseHandler.badRequest(res, "mediaType must be 'movie' or 'tv'");
-    }
-
+    // mediaType is already validated by route middleware
+    
     // Validate mediaId
     const id = parseInt(mediaId);
     if (isNaN(id) || id <= 0) {
