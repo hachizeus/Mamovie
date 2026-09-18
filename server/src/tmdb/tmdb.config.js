127be4339f446623ea4c0b285569d5d1b70e6e99
hachizeus <harchijohns01@gmail.com>
2026-09-18 16:18:36 +0300

    auto-save: update $(date +%s)

--- server/src/tmdb/tmdb.config.js
+++ server/src/tmdb/tmdb.config.js
@@ -1,27 +1,10 @@
-const baseUrl = process.env.TMDB_BASE_URL || "https://api.themoviedb.org/3";
+const baseUrl = "https://api.themoviedb.org/3";
 const token = process.env.TMDB_TOKEN;
 
 const getUrl = (endpoint, params) => {
-  const qs = new URLSearchParams(params);
-  const queryString = qs.toString() ? `&${qs}` : "";
-  return `${baseUrl}/${endpoint}?api_key=${process.env.TMDB_KEY || ""}&${queryString}`.replace(/\?&/, "?");
+  const qs = new URLSearchParams(params || {});
+  const queryString = qs.toString();
+  return `${baseUrl}/${endpoint}${queryString ? `?${queryString}` : ""}`;
 };
 
-// Enhanced version with Bearer token support
-const get = async (url, axiosInstance) => {
-  try {
-    const response = await axiosInstance.get(url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      }
-    });
-    return response.data;
-  } catch (error) {
-    console.error(`TMDB API Error: ${error.message}`, { url, token: token ? "set" : "missing" });
-    throw error;
-  }
-};
-
-export default { getUrl, get, baseUrl, token };
+export default { getUrl, baseUrl, token };
