8c0d3c6bc413ec9a3e62e593973d3b02487fe185
hachizeus <harchijohns01@gmail.com>
2026-09-18 17:18:23 +0300

    auto-save: update $(date +%s)

---
+++ server/src/routes/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import userRoute from "./user.route.js";
+import mediaRoute from "./media.route.js";
+import personRoute from "./person.route.js";
+import reviewRoute from "./review.route.js";
+
+const router = express.Router();
+
+// User routes FIRST (most specific)
+router.use("/user", userRoute);
+
+// Person and review routes (more specific)
+router.use("/:mediaType/:mediaId/person", personRoute);
+router.use("/:mediaType/:mediaId/reviews", reviewRoute);
+
+// Media routes LAST (generic catch-all with params)
+router.use("/:mediaType", mediaRoute);
+
+export default router;
