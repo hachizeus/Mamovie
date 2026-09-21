c33ff12658aae2d3cf544d7b6582cd1fb7527df7
hachizeus <harchijohns01@gmail.com>
2026-09-21 10:37:56 +0300

    auto-save: update $(date +%s)

--- server/src/routes/media.route.js
+++ server/src/routes/media.route.js
@@ -9,15 +9,4 @@ router.get("/genres", mediaController.getGenres);
 router.get("/detail/:mediaId", mediaController.getDetail);
 router.get("/:mediaCategory", mediaController.getList);
 
-// Direct ID fallback - for requests like /movie/969681 (without /detail prefix)
-router.get("/:mediaId", (req, res, next) => {
-  const { mediaId } = req.params;
-  // If it looks like a numeric ID, treat it as detail request
-  if (/^\d+$/.test(mediaId)) {
-    req.params.mediaId = mediaId;
-    return mediaController.getDetail(req, res);
-  }
-  next();
-});
-
 export default router;
