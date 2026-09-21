5a788b49cc5ee544425e23fc57565b2e1455b3dd
hachizeus <harchijohns01@gmail.com>
2026-09-21 08:37:56 +0300

    auto-save: update $(date +%s)

--- server/src/routes/media.route.js
+++ server/src/routes/media.route.js
@@ -55,9 +55,8 @@ router.get(
     .isIn(["popular", "top_rated", "upcoming", "now_playing"])
     .withMessage("mediaCategory must be 'popular', 'top_rated', 'upcoming', or 'now_playing'"),
   query("page")
-    .optional()
-    .toInt()
-    .isInt({ min: 1, max: 1000 }).withMessage("page must be an integer between 1-1000"),
+    .isInt({ min: 1, max: 1000 }).withMessage("page must be an integer between 1-1000")
+    .optional(),
   requestHandler.validate,
   mediaController.getList
 );
