7005f6e669154d87f9128e4e5525c13de9b6c8d0
hachizeus <harchijohns01@gmail.com>
2026-09-21 09:05:35 +0300

    auto-save: update $(date +%s)

--- server/src/tmdb/tmdb.config.js
+++ server/src/tmdb/tmdb.config.js
@@ -2,9 +2,19 @@ const baseUrl = process.env.TMDB_BASE_URL;
 const key = process.env.TMDB_KEY;
 
 const getUrl = (endpoint, params) => {
-  const qs = new URLSearchParams(params);
+  let qs = "";
+  if (params && Object.keys(params).length > 0) {
+    const queryParams = new URLSearchParams();
+    for (const [k, v] of Object.entries(params)) {
+      if (v !== undefined && v !== null) {
+        queryParams.append(k, v);
+      }
+    }
+    qs = queryParams.toString();
+  }
 
-  return `${baseUrl}${endpoint}?api_key=${key}&${qs}`;
+  const separator = qs ? "&" : "?";
+  return `${baseUrl}${endpoint}?api_key=${key}${qs ? separator + qs : ""}`;
 };
 
 export default { getUrl };
