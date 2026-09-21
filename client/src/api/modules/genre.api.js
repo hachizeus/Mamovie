11badcb9dab768624b17bda068f1aba0b7c7c5dc
hachizeus <harchijohns01@gmail.com>
2026-09-21 08:29:41 +0300

    auto-save: update $(date +%s)

--- client/src/api/modules/genre.api.js
+++ client/src/api/modules/genre.api.js
@@ -1,26 +1,13 @@
 import publicClient from "../client/public.client";
-import apiCache from "../utils/cache";
+
+const genreEndpoints = {
+  list: ({ mediaType }) => `genre/${mediaType}/list`
+};
 
 const genreApi = {
   getList: async ({ mediaType }) => {
     try {
-      // Check cache first - genres change rarely, cache for 24 hours
-      const cacheKey = `genres:${mediaType}`;
-      let cached = apiCache.get(cacheKey);
-      
-      if (cached) {
-        return { response: cached };
-      }
-
-      const response = await publicClient.get(
-        `genre/${mediaType}/list`
-      );
-
-      if (response) {
-        // Cache for 24 hours
-        apiCache.set(cacheKey, response);
-      }
-
+      const response = await publicClient.get(genreEndpoints.list({ mediaType }));
       return { response };
     } catch (err) { 
       return { err }; 
