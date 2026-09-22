84c29c58f562c90dad0489c41e6be1ddfc56cd49
hachizeus <harchijohns01@gmail.com>
2026-09-22 10:40:46 +0300

    auto-save: update $(date +%s)

--- client/src/components/common/MediaVideosSlide.jsx
+++ client/src/components/common/MediaVideosSlide.jsx
@@ -67,8 +67,7 @@ const MediaVideo = ({ video }) => {
             border: 0,
             display: "block"
           }}
-          sandbox="allow-same-origin allow-scripts allow-presentation allow-popups allow-popups-to-escape-sandbox"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen; modestbranding"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
           allowFullScreen
           onError={() => {
             console.error("[MediaVideo] iframe error for video:", video.key);
